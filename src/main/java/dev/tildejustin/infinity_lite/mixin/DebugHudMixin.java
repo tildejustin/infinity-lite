caab47faf44038005c635d5a8c94b1f63bf13d9e
tildejustin <spacepepper3.14@gmail.com>
2025-01-27 14:10:15 -0500

    fix remap warning

--- src/main/java/dev/tildejustin/infinity_lite/mixin/DebugHudMixin.java
+++ src/main/java/dev/tildejustin/infinity_lite/mixin/DebugHudMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(DebugHud.class)
 public abstract class DebugHudMixin {
-    @ModifyExpressionValue(method = "getLeftText", at = @At(value = "INVOKE", target = "Lcom/mojang/bridge/game/GameVersion;getName()Ljava/lang/String;"))
+    @ModifyExpressionValue(method = "getLeftText", at = @At(value = "INVOKE", target = "Lcom/mojang/bridge/game/GameVersion;getName()Ljava/lang/String;", remap = false))
     private String addInfiniteBranding(String text) {
         return text + "infinite (lite)";
     }
