6951d2492f4e4fbb00d0ad785bbb4eb70cb44fd0
tildejustin <spacepepper3.14@gmail.com>
2025-01-27 15:41:50 -0500

    user-facing enabled option

--- src/main/java/dev/tildejustin/infinity_lite/InfinityLite.java
+++ src/main/java/dev/tildejustin/infinity_lite/InfinityLite.java
@@ -1,8 +1,17 @@
 package dev.tildejustin.infinity_lite;
 
 import net.minecraft.block.Block;
+import org.mcsr.speedrunapi.config.api.SpeedrunConfig;
+import org.mcsr.speedrunapi.config.api.annotations.Config;
 
-public class InfinityLite {
+public class InfinityLite implements SpeedrunConfig {
+    @Config.Ignored
     public static Block NEITHER_PORTAL;
+
     public static boolean enabled = true;
+
+    @Override
+    public String modID() {
+        return "infinity-lite";
+    }
 }
